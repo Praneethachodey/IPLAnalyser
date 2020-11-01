c3ac3b4e1de472c8eb9ab76d718a29191b1dbdb0
Praneethachodey <praneethachodey@gmail.com>
2020-11-02 01:34:12 +0530

    [Praneetha]Added: Welcome message

---
+++ src/main/java/com/bridgelabz/IPLanalyser/IPLAnalyser.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.IPLanalyser;
+
+public class IPLAnalyser {
+
+	public static void welcomeMessage() {
+		System.out.println("Welcome to IPL Analyser");
+	}
+
+}
---
+++ src/test/java/IPLAnalyser/IPLAnalyserTest.java
@@ -0,0 +1,21 @@
+package IPLAnalyser;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.bridgelabz.IPLanalyser.IPLAnalyser;
+
+public class IPLAnalyserTest {
+	
+	@Before
+	public void welcomeMessage()
+	{
+		IPLAnalyser.welcomeMessage();
+	}
+	
+	@Test
+	public void hello() {
+		IPLAnalyser.welcomeMessage();
+	}
+
+}
